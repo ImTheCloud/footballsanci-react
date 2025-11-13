9fc85128501b84e61744beb8891ee030b0bcff01
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2025-11-13 17:07:38 +0100

    Delete  Section

--- src/sections/History.jsx
+++ src/sections/History.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Section from "../components/Section";
 
 function History() {
-    return (
-        <Section>
-            <h1 className="section-title">History</h1>
-            <p>Coming soon: list of past matches here.</p>
-        </Section>
-    );
+
 }
 
 export default History;
