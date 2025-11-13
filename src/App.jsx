982c10ed6bc55e247e83bda74ba73e349407dc30
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2025-11-13 17:09:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { AuthProvider } from "./components/AuthContext.jsx";
 import { SeasonProvider } from "./components/SeasonContext.jsx";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 
 import Ranking from "./sections/Ranking/Ranking.jsx";
+import Draw from "./sections/Draw/Draw.jsx";
 import History from "./sections/History";
-import Draw from "./sections/Draw/Draw";
 
 function App() {
     return (
         <AuthProvider>
             <SeasonProvider>
+
                 <Header />
 
-                <main style={{ position: "relative", zIndex: 1 }}>
+                <main >
                     <section id="ranking"><Ranking /></section>
                     <section id="draw"><Draw /></section>
                     <section id="history"><History /></section>
@@ -24,6 +26,7 @@ function App() {
                 <Footer />
 
                 <ScrollToTopButton />
+
             </SeasonProvider>
         </AuthProvider>
     );
